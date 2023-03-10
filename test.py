794c5cf72fc9c7b9a4448c675a489ebfb5419649
Iceblok <ycoskun0318@gmail.com>
2023-03-10 10:51:03 +0100

    probleem opgelost betreft products.find({})

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ print("Wat is de naam en prijs van het eerste product in de database?")
 
 def get_first_product():
     """Function returns first product of the database"""
-    first_product = all_products[0]
+    first_product = products.find({})[0]
     first_product_name = first_product["name"]
     first_product_price = first_product["price"]["selling_price"]
     return f"Naam: {first_product_name}\nPrijs: {first_product_price}\n"
@@ -39,7 +39,7 @@ def search_with_first_letter(letter: str):
     @return: str, output RCF V-MAX V35 passieve 15 inch luidspreker 3600W
     """
     letter = letter.upper()
-    for product in all_products:
+    for product in products.find({}):
         product_name = product["name"]
         first_letter_of_product = product_name[0].upper()
         if first_letter_of_product == letter:
@@ -56,7 +56,7 @@ print("Wat is de gemiddelde prijs van de producten in de database?")
 def average_price():
     """Function returns the average price of the products out of the database"""
     price_list = []
-    for product in all_products:
+    for product in products.find({}):
         price_list.append(product["price"]["selling_price"])
     return sum(price_list) / len(price_list)
 
